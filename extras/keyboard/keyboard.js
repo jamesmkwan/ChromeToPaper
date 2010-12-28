b339de87effaba6a1270a35584d829aa314b78c3
unknown <James@.(none)>
2010-12-28 00:53:52 -0800

    Updated for new version of Instapaper Extras: Keyboard Shortcuts
    The new version allows you to specify a binding to open the Instapaper website.
    In addition, security of extras in the main extension has been enhanced.
    In the options page, the extension will list all extras, even those which have not made requests.
    That way, if a rogue extension is trying to mess with this extension, it will appear in the options panel.
    The user would be able to track down the culprit with the extension ID.
    In addition, the number of requests (including bogus ones) is counted and tallied up.
    For instance, if a buggy extra is spamming requests, it can easily be tracked down.
    This is also good for debugging purposes to check if an extra is communicating with the main extension.

--- extras/keyboard/keyboard.js
+++ extras/keyboard/keyboard.js
@@ -1,13 +1,17 @@
-var instapaper_id='oeldmfnjmankppagkkmfkmhmbldeamjd';
-
 chrome.extension.sendRequest({
   'task':'shortcut'
 },function(x) {
   document.addEventListener('keyup',function(e) {
-    if(''.concat(e.ctrlKey+0,e.shiftKey+0,e.altKey+0,e.which)==x) {
-      chrome.extension.sendRequest(instapaper_id,{
-        'task':'saveToInstapaper'
-      });
+    var s=''.concat(e.ctrlKey+0,e.shiftKey+0,e.altKey+0,e.which);
+    var d=x.d;
+    for(j in d) {
+      if(s==d[j]) {
+        for(var i=0;i<x.id.length;i++) {
+          chrome.extension.sendRequest(x.id[i],{
+            'task':j
+          });
+        }
+      }
     }
   });
 });
