7f0f462d730c0887d4362286e8bbb7c625900baf
James Kwan <jamesmkwan.com@gmail.com>
2011-01-14 18:50:11 -0800

    Cleaned Junk Files

--- core/kSendKey.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  if(document.getElementById('form_key_send_now')) {
-    chrome.extension.sendRequest({
-      'task':'kSendKey',
-      'ksk':document.getElementById('form_key_send_now').value
-    });
-  }
-})();
