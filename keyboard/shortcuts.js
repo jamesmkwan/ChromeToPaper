27c57ad9ab05e2887a51f7c09ae4a8374d99f075
James Kwan <jamesmkwan.com@gmail.com>
2011-04-23 14:18:03 -0700

    Placed the shortcuts data in a JSON file

---
+++ keyboard/shortcuts.js
@@ -0,0 +1,12 @@
+var shortcuts={
+  'opt_saveToInstapaper': {
+    'name':'ChromeToPaper',
+    'task':'saveToInstapaper',
+    'description':'Activating the keystroke for this action is equivalent to clicking the Browser Button.'
+  },
+  'opt_openInstapaperList': {
+    'name':'Open Reading List',
+    'task':'openInstapaperList',
+    'description':'Opens http://www.instapaper.com/u in a new tab'
+  }
+}
