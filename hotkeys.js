eba7b8c399acf86c989ddca0b622300c0dba2f4d
James Kwan <jamesmkwan.com@gmail.com>
2012-09-11 20:41:11 -0700

    Hotkeys.js updated from the old ChromeToPaper Keyboard Shortcuts

---
+++ hotkeys.js
@@ -0,0 +1,14 @@
+chrome.extension.sendMessage({
+  'task': 'hotkeys'
+}, function(resp) {
+  document.addEventListener('keyup',function(e) {
+    var hotkey=''.concat(e.ctrlKey + 0, e.shiftKey + 0, e.altKey + 0, e.which);
+    for(i in resp) {
+    	if(resp[i]['hotkey'] == hotkey) {
+  		  chrome.extension.sendMessage({
+          'task': resp[i]['task']
+        });
+    	}
+    }
+  });
+});
