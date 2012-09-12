965a8a638ce6b21a9c01c2c17e5b93a7378ac79d
James Kwan <jamesmkwan.com@gmail.com>
2012-09-11 20:36:58 -0700

    ChromeToPaper is now just one extesnion

--- core/reader.js
+++
@@ -1,7 +0,0 @@
-//To Implement: Google Reader -> Instapaper
-
-chrome.extension.onRequest.addListener(function(req,sender,callback) {
-  if(document.getElementById('current-entry')) {
-  } else {
-  }
-});
